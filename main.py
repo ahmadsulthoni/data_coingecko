9a932f6fa2a46d794f6a1dac0e10e686857908db
anonimus <ahmadsulthoni.ext@gmail.com>
2022-03-23 19:53:30 +0700

    Commit pertama

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-import os
-
 import requests
 from bs4 import BeautifulSoup
-
 url = 'https://www.coingecko.com/id'
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'
 }
 
-result = []
-
-res = requests.get(url, headers=headers)
 r = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(r.content, 'html.parser')
-content = soup.findAll('gecko-table-container')
 
-try:
-    os.mkdir('Hasil_data')
-except FileExistsError:
-    pass
-
-#proses scraping
+headers_content = soup.find_all('div','tw-flex tw-items-center')
+print(headers_content)
